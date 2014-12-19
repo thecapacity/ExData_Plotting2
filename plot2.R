950143ed9d792678f38357de29263126d6555a79
jay <thecapacity@gmail.com>
2014-12-19 14:58:04 -0500

    Fixed Comment to reflect Q2

--- plot2.R
+++ plot2.R
@@ -1,8 +1,7 @@
 ## This code and associated plot explores the following question:
 ##
-## Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
-## Using the base plotting system, make a plot showing the total PM2.5 emission from 
-## all sources for each of the years 1999, 2002, 2005, and 2008.
+## Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510")
+## from 1999 to 2008? Use the base plotting system to make a plot answering this question.
 
 source("load_data.R")
 ## Loads two data sets, emissions_data and classification_data
