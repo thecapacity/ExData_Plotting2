06f76bb96d2dcf650b581295544dfee15de8cf2a
jay <thecapacity@gmail.com>
2014-12-19 14:48:49 -0500

    First Cut at Question 1
    Code and Image

--- plot1.R
+++ plot1.R
@@ -7,13 +7,28 @@
 source("load_data.R")
 ## Loads two data sets, emissions_data and classification_data
 
-select <- emissions_data$year == 1999 | emissions_data$year == 2002 | emissions_data$year == 2005 | emissions_data$year == 2008
+## Subset Data, just to be sure)
+selection <- subset(emissions_data, year == 1999 | year == 2002 | year == 2005 | year == 2008)
+##selection <- emissions_data[emissions_data$year == 1999 | emissions_data$year == 2002 | emissions_data$year == 2005 | emissions_data$year == 2008,]
+
+## Totals
+totals <- data.frame( row.names=c("1999", "2002", "2005", "2008"), totals=c(
+                            sum(selection$Emissions[ which(selection$year == 1999) ]), #1999
+                            sum(selection$Emissions[ which(selection$year == 2002) ]), #2002
+                            sum(selection$Emissions[ which(selection$year == 2005) ]), #2005
+                            sum(selection$Emissions[ which(selection$year == 2008) ]) #2008
+                        ) )
 
 #Output graph to file
-#png(filename = "plot1.png", width=480, height=480, units="px", bg="white") #bg="transparent")
+png(filename = "plot1.png", width=480, height=480, units="px", bg="white") #bg="transparent")
 
-plot(emissions_data[select,]$Emissions ~ emissions_data[select,]$year, 
-        xlab="year", ylab="Emission Levels", type="l")
+plot(rownames(totals), totals$totals, type="l",
+        xlab="Year", 
+        ylab="Emission Levels", axes=FALSE)
+title("Total US PM2.5 Emissions by Year")
+axis(1, at=rownames(totals))
+axis(2, at=totals$totals)
+box()
 
 #Close device
 dev.off()
